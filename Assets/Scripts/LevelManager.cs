e282a21a55fd2b8e736de959ccf4d39a95807a5a
Pablo <pabloimrik17@gmail.com>
2016-07-19 19:22:35 +0200

    Splash Screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	private int currentLevel;
+    public float autoLoadNextLevelAfter;
+
+    void Start() {
+        Invoke("LoadNextLevel",autoLoadNextLevelAfter);
+    }
+
+	public void LoadLevel (string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+
+	public void QuitRequest ()
+	{
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
