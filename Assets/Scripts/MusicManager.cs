93feba001b155ed39633d24235ffea0b1abfafa9
Pablo <pabloimrik17@gmail.com>
2016-07-21 21:09:49 +0200

    Music Manager Win, Loose and Options Screens

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+	// Use this for initialization
+	void Start () {
+	    audioSource = gameObject.GetComponent<AudioSource>();
+        playLevelAudio(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  void OnLevelWasLoaded(int nivel) {
+        playLevelAudio(nivel);
+  }
+
+  private void playLevelAudio(int audioIndex) {   
+    if (levelMusicChangeArray[audioIndex]) {
+      AudioClip thisLevelAudio = levelMusicChangeArray[audioIndex];
+      audioSource.clip = thisLevelAudio;
+      audioSource.loop = true;
+      audioSource.Play();
+    }
+  }
+}
